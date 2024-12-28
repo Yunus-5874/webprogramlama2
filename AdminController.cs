4785f3c25732dcb9720a7e844708c76398222c63
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:28:37 +0300

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BerberWebSitesi.Data;
+using BerberWebSitesi.Models;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace BerberWebSitesi.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly BerberContext _context;
+
+        public AdminController(BerberContext context)
+        {
+            _context = context;
+        }
+
+        // Admin giriş sayfasını göster
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var admin = _context.Admins.FirstOrDefault(a => a.Username == username && a.Password == password);
+
+            if (admin != null)
+            {
+                HttpContext.Session.SetString("Admin", "LoggedIn");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Kullanıcı adı veya şifre hatalı.";
+                return View();
+            }
+        }
+
+        // Admin paneli ana sayfası
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Admin") != "LoggedIn")
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        // Admin çıkışı
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
+
